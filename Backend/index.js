884c7e7c7bc43a6b508c3c0b88054a0cac0a9318
maharsh_nayak <64581149+Maharsh-Nayak@users.noreply.github.com>
2025-02-22 12:35:49 +0530

    Setup the project

---
+++ Backend/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+let port = process.env.port || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
