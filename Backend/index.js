fce3b4569310a942ff7af12ff80563266d340ab7
Madhurq <madhur.mu5@Gmail.com>
2025-02-23 16:17:37 +0530

    no warnings on nodemon

--- Backend/index.js
+++ Backend/index.js
@@ -40,8 +40,8 @@ app.use(session({
 // })
 
 connect('mongodb+srv://Userdb:whyshouldisay1@cluster0.uuehk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
 })
 .then(() => console.log("✅ Connected to MongoDB"))
 .catch((err) => console.error("❌ MongoDB Connection Error:", err));
